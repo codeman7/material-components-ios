8322fa86a086512f9d224849c0d06d6a848d45ca
Cody Weaver <codyleeweaver@gmail.com>
2019-11-04 11:14:53 -0800

    In viewDidLayoutSubviews we get the correct behavior

--- components/BottomSheet/examples/BottomSheetCodyExample.swift
+++ components/BottomSheet/examples/BottomSheetCodyExample.swift
@@ -1,49 +1,34 @@
 import UIKit
 import MaterialComponents
 
+@available(iOS 10.0, *)
 class ExampleCode: UIViewController {
-  let presentButton = UIButton()
   let label = UILabel()
+  let customView = CustomView()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    presentButton.setTitle("Present", for: .normal)
-    presentButton.setTitleColor(.black, for: .normal)
-    presentButton.backgroundColor = .white
-    presentButton.sizeToFit()
-    presentButton.addTarget(self, action: #selector(presentBottomSheet), for: .touchUpInside)
     view.backgroundColor = .white
-    //view.addSubview(presentButton)
-    label.font = UIFont.preferredFont(forTextStyle: .body)
+    customView.label.font = UIFont.preferredFont(forTextStyle: .body)
     if #available(iOS 11.0, *) {
-      label.adjustsFontForContentSizeCategory = true
+      customView.label.adjustsFontForContentSizeCategory = true
     }
-    label.textColor = .black
-    label.text = "Foo"
-    view.addSubview(label)
+    customView.label.textColor = .black
+    customView.label.text = "Foo"
+    view.addSubview(customView)
   }
 
-  override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
 
-    //presentButton.center = view.center
-
-    label.sizeToFit()
-    label.center = view.center
-    if #available(iOS 11.0, *) {
-      let size = label.bounds.height
-      print("Traitcollection.preferred = \(traitCollection.preferredContentSizeCategory) sizeThatFits: \(size)")
-    }
-  }
-
-  @objc func presentBottomSheet() {
-    let bottomSheet = CodyBottomSheet()
-    present(bottomSheet, animated: true, completion: nil)
+    customView.sizeToFit()
+    customView.center = view.center
   }
 }
 
 // MARK: Catalog by convention
+@available(iOS 10.0, *)
 extension ExampleCode {
 
   @objc class func catalogMetadata() -> [String: Any] {
@@ -51,87 +36,46 @@ extension ExampleCode {
       "breadcrumbs": ["Bottom Sheet", "Cody"],
       "primaryDemo": false,
       "presentable": true,
-      "debug": true,
     ]
   }
 }
 
+@available(iOS 10.0, *)
+class CustomView: UIView, UIContentSizeCategoryAdjusting {
+  var adjustsFontForContentSizeCategory: Bool {
+    get {
+      return label.adjustsFontForContentSizeCategory
+    }
 
-class CodyBottomSheet: UIViewController {
-  let transitionController = MDCBottomSheetTransitionController()
-  var expanded: Bool = false
-  var cellNumber: Int {
-    return expanded ? 100 : 5
+    set {
+      label.adjustsFontForContentSizeCategory = newValue
+    }
   }
-  let tableView = UITableView()
+
+  let label = UILabel()
+
+  override func sizeThatFits(_ size: CGSize) -> CGSize {
+    return label.sizeThatFits(size)
+  }
 
   init() {
-    super.init(nibName: nil, bundle: nil)
-    transitionController.dismissOnBackgroundTap = true
-    super.transitioningDelegate = transitionController
-    super.modalPresentationStyle = .custom
-    self.view.backgroundColor = .white
-    transitionController.trackingScrollView = tableView
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellTypeIdentifier")
-    tableView.delegate = self
-    tableView.dataSource = self
+    super.init(frame: .zero)
+    label.text = ""
+    addSubview(label)
+
   }
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
+  override func layoutSubviews() {
+    super.layoutSubviews()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    view.addSubview(tableView)
-  }
-
-  override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
-
-    if let bottom = presentationController as? MDCBottomSheetPresentationController {
-      let height: CGFloat
-      if expanded {
-        tableView.contentOffset = CGPoint(x: 0, y: -77)
-        tableView.contentInset = UIEdgeInsets(top: 77, left: 0, bottom: 0, right: 0)
-        height = view.bounds.height / 2
-      } else {
-        tableView.contentOffset = CGPoint(x: 0, y: 0)
-        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        height = tableView.contentSize.height
-      }
-      print("Height = \(height)")
-      bottom.preferredSheetHeight = height
-    }
-    print("tableView.contentSize = \(tableView.contentSize)")
-    tableView.frame = view.bounds
-  }
-}
-
-extension CodyBottomSheet: UITableViewDataSource {
-  // Return the number of rows for the table.
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     return cellNumber
-  }
-
-  // Provide a cell object for each row.
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     // Fetch a cell of the appropriate type.
-     let cell = tableView.dequeueReusableCell(withIdentifier: "cellTypeIdentifier", for: indexPath)
-
-     // Configure the cell’s contents.
-    cell.textLabel!.text = "Cell #\(indexPath.row)"
-
-     return cell
-  }
-}
-
-extension CodyBottomSheet: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    expanded = !expanded
-    tableView.reloadData()
-    view.setNeedsLayout()
+    let firstSize = label.sizeThatFits(bounds.size)
+    print("Label sizeThatFits first = \(firstSize)")
+    label.frame = bounds
+    let secondarySize = label.sizeThatFits(bounds.size)
+    print("label sizeThatFits = \(secondarySize)")
   }
 }
